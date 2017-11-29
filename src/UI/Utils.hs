51711788153486832dac4a3c0a56ea8d56b92381
etomov <emil.tomov@rewe-digital.com>
2017-11-29 10:56:53 +0200

    Fixing not whole message is ent to kafka leading to JsonEOFException in consumers (e.g. not valid json (missing closing bracket,tags etc))

--- src/UI/Utils.hs
+++ src/UI/Utils.hs
@@ -1,21 +1,21 @@
 module UI.Utils
-    ( mkButton
-    , renderProducerError
-    , renderConsumerError
-    , getTextFromTextBuffer
-    , getUUIDAsString
-    , KafkaError
-    , stringToByteStr
-    , byteStringToString
-    , validate
-    , report
-    , process
-    , updateStatusBar
-    , timestamp
-    , formattedTimeStamp
-    , debugMessage
-    , getSpinButtonValue
-    ) where
+  ( mkButton
+  , renderProducerError
+  , renderConsumerError
+  , getTextFromTextBuffer
+  , getUUIDAsString
+  , KafkaError
+  , stringToByteStr
+  , byteStringToString
+  , validate
+  , report
+  , process
+  , updateStatusBar
+  , timestamp
+  , formattedTimeStamp
+  , debugMessage
+  , getSpinButtonValue
+  ) where
 
 import qualified Data.ByteString.UTF8             as BU
 import           Data.Time
@@ -27,7 +27,8 @@ import           Graphics.UI.Gtk                  (AttrOp ((:=)), ContextId,
                                                    statusbarPop, statusbarPush,
                                                    textBufferGetIterAtLine,
                                                    textBufferGetLineCount,
-                                                   textBufferGetText)
+                                                   textBufferGetSlice,
+                                                   textBufferGetBounds)
 import           Graphics.UI.Gtk.Buttons.Button
 import           Graphics.UI.Gtk.Entry.SpinButton
 import           Kafka.Consumer.Types
@@ -36,43 +37,41 @@ import           Kafka.Types
 -- | Create a button and attach handler to it that mutates calculator's
 -- state with given function.
 mkButton ::
-       String -- ^ Button label
-    -> IO Button -- ^ Resulting button object
+     String -- ^ Button label
+  -> IO Button -- ^ Resulting button object
 mkButton label = do
-    btn <- buttonNew
-    set btn [buttonLabel := label]
-    return btn
+  btn <- buttonNew
+  set btn [buttonLabel := label]
+  return btn
 
 -- | Render given 'Value'.
 renderProducerError :: Either KafkaError () -> String
 renderProducerError err =
-    case err of
-        Left val  -> "Kafka Said - " ++ show err
-        Right val -> "Send Succeeded"
+  case err of
+    Left val  -> "Kafka Said - " ++ show err
+    Right val -> "Send Succeeded"
 
 renderConsumerError :: Either KafkaError (ConsumerRecord (Maybe BU.ByteString) (Maybe BU.ByteString)) -> String
 renderConsumerError err =
-    case err of
-        Left val  -> "Kafka Said - " ++ show err
-        Right val -> "Read Succeeded"
+  case err of
+    Left val  -> "Kafka Said - " ++ show err
+    Right val -> "Read Succeeded"
 
 getTextFromTextBuffer :: TextBuffer -> IO String
 getTextFromTextBuffer b = do
-    lcnt <- textBufferGetLineCount b
-    bBeginIt <- textBufferGetIterAtLine b 0
-    bEndIt <- textBufferGetIterAtLine b lcnt
-    textBufferGetText b bBeginIt bEndIt True
+  (bBeginIt,bEndIt) <- textBufferGetBounds b
+  textBufferGetSlice b bBeginIt bEndIt True
 
 getUUIDAsString :: IO String
 getUUIDAsString = do
-    uuid <- UUID.V1.nextUUID
-    return $ f uuid
+  uuid <- UUID.V1.nextUUID
+  return $ f uuid
   where
     f :: Maybe UUID.UUID -> String
     f u =
-        case u of
-            Just u' -> UUID.toString u'
-            Nothing -> "meh-meh-beh"
+      case u of
+        Just u' -> UUID.toString u'
+        Nothing -> "meh-meh-beh"
 
 stringToByteStr :: String -> BU.ByteString
 stringToByteStr = BU.fromString
@@ -82,10 +81,10 @@ byteStringToString = BU.toString
 
 process :: IO String -> String -> IO (Bool, String, String)
 process inputValue errMsg = do
-    value <- inputValue
-    v1 <- validate inputValue
-    errorMsg <- report v1 errMsg
-    return (v1, errorMsg, value)
+  value <- inputValue
+  v1 <- validate inputValue
+  errorMsg <- report v1 errMsg
+  return (v1, errorMsg, value)
 
 report :: Bool -> String -> IO String
 report False msg = return msg
@@ -93,41 +92,40 @@ report True msg  = return ""
 
 validate :: IO String -> IO Bool
 validate inputValue = do
-    eText <- inputValue
-    return (not $ null eText)
+  eText <- inputValue
+  return (not $ null eText)
 
 updateStatusBar :: Statusbar -> ContextId -> String -> IO MessageId
 updateStatusBar statusBar statusBarId msg = do
-    timeNow <- formattedTimeStamp
-    statusbarPop statusBar statusBarId
-    statusbarPush statusBar statusBarId $ timeNow ++ msg
+  timeNow <- formattedTimeStamp
+  statusbarPop statusBar statusBarId
+  statusbarPush statusBar statusBarId $ timeNow ++ msg
 
 -- time utils
 timestamp :: IO String
 timestamp = do
-    zonedTime <- getZonedTime
-    return $ insertColon $ formattedZonedTimeNow zonedTime
+  zonedTime <- getZonedTime
+  return $ insertColon $ formattedZonedTimeNow zonedTime
   where
     formattedZonedTimeNow :: ZonedTime -> String
-    formattedZonedTimeNow = formatTime defaultTimeLocale $ iso8601DateFormat (Just"%T%z")
-
-    insertColon str = take ((-) (length str) 2) str ++ (colonOrEmptyStr $ getCharAt str 3 ) ++ drop ((-) (length str) 2) str
+    formattedZonedTimeNow = formatTime defaultTimeLocale $ iso8601DateFormat (Just "%T%z")
+    insertColon str = take ((-) (length str) 2) str ++ (colonOrEmptyStr $ getCharAt str 3) ++ drop ((-) (length str) 2) str
     getCharAt tt n = (tt !! ((-) (length tt) n))
     colonOrEmptyStr ':' = ""
-    colonOrEmptyStr _ = ":"
+    colonOrEmptyStr _   = ":"
 
 formattedTimeStamp :: IO String
 formattedTimeStamp = do
-    timeNow <- timestamp
-    return $ "[" ++ timeNow ++ "]"
+  timeNow <- timestamp
+  return $ "[" ++ timeNow ++ "]"
 
 debugMessage :: String -> IO ()
 debugMessage message = do
-    beginTime <- formattedTimeStamp
-    putStrLn $ beginTime ++ message
-    return ()
+  beginTime <- formattedTimeStamp
+  putStrLn $ beginTime ++ message
+  return ()
 
 getSpinButtonValue :: SpinButton -> IO Double
 getSpinButtonValue btn = do
-    spinButtonUpdate btn
-    spinButtonGetValue btn
+  spinButtonUpdate btn
+  spinButtonGetValue btn
